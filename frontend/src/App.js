7e20e2082c3ce0f34b9f2fcbd5f07ec7d9275041
Basantraj.shakti <basantrajshakti@gmail.com>
2024-10-15 21:58:29 +0530

    Project created with assessment boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import { PipelineToolbar } from './Toolbar';
+import { PipelineUI } from './UI';
+import { SubmitButton } from './Submit';
+
+function App() {
+  return (
+    <div>
+      <PipelineToolbar />
+      <PipelineUI />
+      <SubmitButton />
+    </div>
+  );
+}
+
+export default App;
